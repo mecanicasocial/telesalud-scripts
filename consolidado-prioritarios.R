3c30c9be209a5b97c78a44098f2327fa8450c83c
davasquezg <dr.vasquez@msocial.health>
2025-01-28 21:17:47 -0300

    Mejoras en el script consolidado-prioritarios: manejo de errores y uso de dplyr

--- consolidado-prioritarios.R
+++ consolidado-prioritarios.R
@@ -13,23 +13,36 @@ if (!require("tidyverse")) {
 library(readxl)
 library(conflicted)
 library(tidyverse)
+
 # Definir la ruta del archivo
 ruta_archivo <- file.path(Sys.getenv("USERPROFILE"), "Downloads", "TodasLasSolicitudesPendientes.xlsx")
-datos <- read_excel(ruta_archivo)
 
-#Comandos de busqueda Vicio Refraccion
-lentes_search <- datos[grepl("lent", datos$`Información adicional`, ignore.case = TRUE), ]
-ametropia_search <- datos[grepl("vici", datos$`Información adicional`, ignore.case = TRUE), ]
-cons_vicio <- bind_rows(lentes_search, ametropia_search)
-cons_vicio_limpio <- cons_vicio %>% distinct(`Nº identificador`, .keep_all = TRUE) %>% arrange(`Nº identificador`)
+# Manejar posibles errores al leer el archivo
+tryCatch({
+  datos <- read_excel(ruta_archivo)
+}, error = function(e) {
+  cat("Error al leer el archivo de Excel:", e$message, "\n")
+})
+
+# Comandos de busqueda Vicio Refraccion
+cons_vicio <- datos %>%
+  filter(grepl("lent", `Información adicional`, ignore.case = TRUE) |
+         grepl("vici", `Información adicional`, ignore.case = TRUE)) %>%
+  distinct(`Nº identificador`, .keep_all = TRUE) %>%
+  arrange(`Nº identificador`)
+
 cat("Resultados Vicio\n")
-for (id in cons_vicio_limpio$`Nº identificador`) {
+for (id in cons_vicio$`Nº identificador`) {
   cat(id, "\n")
 }
 
-#Comandos de busqueda LME
-md_casos <- datos[grepl("Medicina", datos$`Tipo prestador`, ignore.case = FALSE), ]
-lme_search <- datos[grepl("licencia", datos$`Información adicional`, ignore.case = TRUE), ]
+# Comandos de busqueda LME
+md_casos <- datos %>%
+  filter(grepl("Medicina", `Tipo prestador`, ignore.case = FALSE))
+
+lme_search <- datos %>%
+  filter(grepl("licencia", `Información adicional`, ignore.case = TRUE))
+
 cat("Resultados LME\n")
 for (id in lme_search$`Nº identificador`) {
   cat(id, "\n")
