97032ea0fa1656357af41257b9a60b08c05feeae
davasquezg <dr.vasquez@msocial.health>
2025-02-23 23:53:15 -0300

    Refactor scripts: streamline package installation and enhance search functionality

---
+++ GES.R
@@ -0,0 +1,30 @@
+# Instalar y cargar el paquete necesario
+if (!require("readxl")) {
+  install.packages("readxl")
+}
+if (!require("conflicted")) {
+  devtools::install_github("r-lib/conflicted")
+}
+conflicts_prefer(dplyr::filter)
+conflicts_prefer(dplyr::lag)
+if (!require("tidyverse")) {
+  install.packages("tidyverse")
+}
+library(readxl)
+library(conflicted)
+library(tidyverse)
+# Definir la ruta del archivo
+ruta_archivo <- file.path(Sys.getenv("USERPROFILE"), "Downloads", "TodasLasSolicitudesPendientes.xlsx")
+datos <- read_excel(ruta_archivo)
+#Comandos de busqueda
+md_casos <- datos[grepl("Medicina", datos$`Tipo prestador`, ignore.case = FALSE), ]
+ges_search <- datos[grepl("GES", datos$`Información adicional`, ignore.case = TRUE), ] %>% arrange(`Fecha solicitud`)
+# Resultados
+cat("Resultados GES\n")
+if (nrow(ges_search) == 0) {
+  cat("No se encontraron resultados para GES\n")
+} else {
+  for (id in ges_search$`Nº identificador`) {
+    cat(id, "\n")
+  }
+}
--- consolidado-prioritarios.R
+++ consolidado-prioritarios.R
@@ -1,18 +1,14 @@
-# Instalar y cargar el paquete necesario
-if (!require("readxl")) {
-  install.packages("readxl")
-}
-if (!require("conflicted")) {
-  devtools::install_github("r-lib/conflicted")
-}
-conflicts_prefer(dplyr::filter)
-conflicts_prefer(dplyr::lag)
-if (!require("tidyverse")) {
-  install.packages("tidyverse")
-}
+# Instalar y cargar los paquetes necesarios
+required_packages <- c("readxl", "conflicted", "tidyverse")
+new_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages)
+
 library(readxl)
 library(conflicted)
 library(tidyverse)
+
+conflicts_prefer(dplyr::filter)
+conflicts_prefer(dplyr::lag)
 
 # Definir la ruta del archivo
 ruta_archivo <- file.path(Sys.getenv("USERPROFILE"), "Downloads", "TodasLasSolicitudesPendientes.xlsx")
@@ -24,36 +20,20 @@ tryCatch({
   cat("Error al leer el archivo de Excel:", e$message, "\n")
 })
 
-# Comandos de busqueda Vicio Refraccion
-cons_vicio <- datos %>%
-  filter(grepl("lent", `Información adicional`, ignore.case = TRUE) |
-         grepl("vici", `Información adicional`, ignore.case = TRUE)) %>%
-  distinct(`Nº identificador`, .keep_all = TRUE) %>%
-  arrange(`Fecha solicitud`)
-
-# Resultados
-
-cat("Resultados Vicio\n")
-if (nrow(cons_vicio) == 0) {
-  cat("No se encontraron resultados.\n")
-} else {
-  for (id in cons_vicio$`Nº identificador`) {
-    cat(id, "\n")
+# Función para realizar la búsqueda y mostrar resultados
+buscar_y_mostrar_resultados <- function(datos, patron, mensaje) {
+  resultados <- datos[grepl(patron, datos$`Información adicional`, ignore.case = TRUE), ] %>% arrange(`Fecha solicitud`)
+  cat(mensaje, "\n")
+  if (nrow(resultados) == 0) {
+    cat("No se encontraron resultados.\n")
+  } else {
+    for (id in resultados$`Nº identificador`) {
+      cat(id, "\n")
+    }
   }
 }
 
-# Comandos de busqueda LME
-md_casos <- datos %>%
-  filter(grepl("Medicina", `Tipo prestador`, ignore.case = FALSE))
-
-lme_search <- datos[grepl("licencia", datos$`Información adicional`, ignore.case = TRUE), ] %>% arrange(`Fecha solicitud`)
-
-# Resultados
-cat("Resultados LME\n")
-if (nrow(lme_search) == 0) {
-  cat("No se encontraron resultados para LME\n")
-} else {
-  for (id in lme_search$`Nº identificador`) {
-    cat(id, "\n")
-  }
-}
+# Comandos de búsqueda y resultados
+buscar_y_mostrar_resultados(datos, "lent|vici", "Resultados Vicio")
+buscar_y_mostrar_resultados(datos, "licencia", "Resultados LME")
+buscar_y_mostrar_resultados(datos, "GES", "Resultados GES")
